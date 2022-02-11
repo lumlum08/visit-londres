f69259e7bcbcbf23e5b6365eb28640a5756c8beb
georgia drai <djonadre@gmail.com>
2022-02-11 15:13:00 +0100

    add map and marker et photo successfully

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,3 +1,5 @@
 class Visit < ApplicationRecord
   validates :name, :description, :location,  presence: true
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
