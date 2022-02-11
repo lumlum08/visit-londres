c8db20e2b103bef10b4d89035606a39343ee001d
georgia drai <djonadre@gmail.com>
2022-02-11 20:22:17 +0100

    search bar ne fonctionne pas encore

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_000411) do
+ActiveRecord::Schema.define(version: 2022_02_11_155019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_02_11_000411) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "category"
   end
 
 end
